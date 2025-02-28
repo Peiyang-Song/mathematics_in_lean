289ed7ebcc57c57312311691058f07861e01d626
psong <psong@caltech.edu>
2025-02-28 13:05:16 -0800

    C02 S01

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -7,10 +7,28 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
 
 -- Try these.
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  -- suggest_tactics
+  -- ring
+
+  -- search_proof
+  -- ring
+
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm c a]
+  -- aesop
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  -- suggest_tactics
+  -- ring
+
+  -- search_proof
+  -- ring
+
+  rw [← mul_assoc a b c]
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
+  -- aesop
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
@@ -20,10 +38,27 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
 /- Try doing the first of these without providing any arguments at all,
    and the second with only one argument. -/
 example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
-  sorry
+  -- suggest_tactics
+  -- ring
+
+  -- search_proof
+  -- ring
+
+  rw [mul_comm]
+  rw [mul_assoc]
+  -- aesop
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  -- suggest_tactics
+  -- ring
+
+  -- search_proof
+  -- ring
+
+  rw [← mul_assoc]
+  rw [mul_comm a]
+  rw [mul_assoc]
+  -- aesop
 
 -- Using facts from the local context.
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
@@ -33,10 +68,31 @@ example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c *
   rw [mul_assoc]
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  sorry
+  -- suggest_tactics
+  -- simp [h, mul_assoc]
+
+  -- search_proof
+  -- simp_all only [mul_eq_mul_right_iff]
+  -- simp [h, mul_assoc]
+
+  rw [mul_assoc a]
+  rw [h]
+  rw [← mul_assoc]
+  -- aesop
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  -- suggest_tactics
+  -- linarith
+
+  -- search_proof
+  -- subst hyp' hyp
+  -- ring
+
+  rw [hyp]
+  rw [hyp']
+  rw [mul_comm]
+  -- aesop
+  rw [sub_self]
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
@@ -84,11 +140,11 @@ example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
 example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
   calc
     (a + b) * (a + b) = a * a + b * a + (a * b + b * b) := by
-      sorry
+      sorry -- No solution provided.
     _ = a * a + (b * a + a * b) + b * b := by
-      sorry
+      sorry -- No solution provided.
     _ = a * a + 2 * (a * b) + b * b := by
-      sorry
+      sorry -- No solution provided.
 
 end
 
@@ -97,10 +153,10 @@ section
 variable (a b c d : ℝ)
 
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  sorry -- No solution provided.
 
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+  sorry -- No solution provided.
 
 #check pow_two a
 #check mul_sub a b c
