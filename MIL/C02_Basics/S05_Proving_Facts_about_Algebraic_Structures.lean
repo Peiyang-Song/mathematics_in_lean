0fd15e57c505580efef7a7fe705adeef872cfa24
psong <psong@caltech.edu>
2025-02-28 13:07:21 -0800

    C02 S05

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,22 +36,124 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  -- suggest_tactics
+  -- rw [inf_comm]
+
+  -- search_proof
+  -- rw [inf_comm]
+
+  apply le_antisymm
+  repeat
+    apply le_inf
+    · apply inf_le_right
+    apply inf_le_left
+
+  -- aesop
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  -- suggest_tactics
+  -- ac_rfl
+
+  -- search_proof
+  -- ac_rfl
+
+  apply le_antisymm
+  · apply le_inf
+    · trans x ⊓ y
+      apply inf_le_left
+      apply inf_le_left
+    apply le_inf
+    · trans x ⊓ y
+      apply inf_le_left
+      apply inf_le_right
+    apply inf_le_right
+  apply le_inf
+  · apply le_inf
+    · apply inf_le_left
+    trans y ⊓ z
+    apply inf_le_right
+    apply inf_le_left
+  trans y ⊓ z
+  apply inf_le_right
+  -- apply inf_le_right
+  aesop
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  -- suggest_tactics
+  -- rw [sup_comm]
+
+  -- search_proof
+  -- rw [sup_comm]
+
+  apply le_antisymm
+  repeat
+    apply sup_le
+    · apply le_sup_right
+    apply le_sup_left
+
+  -- aesop
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  -- suggest_tactics
+  -- ac_rfl
+
+  -- search_proof
+  -- ac_rfl
+
+  apply le_antisymm
+  · apply sup_le
+    · apply sup_le
+      apply le_sup_left
+      · trans y ⊔ z
+        apply le_sup_left
+        apply le_sup_right
+    trans y ⊔ z
+    apply le_sup_right
+    apply le_sup_right
+  apply sup_le
+  · trans x ⊔ y
+    apply le_sup_left
+    apply le_sup_left
+  apply sup_le
+  · trans x ⊔ y
+    apply le_sup_right
+
+  --   aesop
+  -- aesop
+
+    apply le_sup_left
+  apply le_sup_right
+
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  -- suggest_tactics
+  -- simp
+
+  -- search_proof
+  -- simp_all only [le_sup_left, inf_of_le_left]
+
+  -- aesop
+
+  apply le_antisymm
+  · apply inf_le_left
+  apply le_inf
+  · apply le_refl
+  apply le_sup_left
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  -- suggest_tactics
+  -- simp
+
+  -- search_proof
+  -- simp_all only [inf_le_left, sup_of_le_left]
+
+  -- aesop
+
+  apply le_antisymm
+  · apply sup_le
+    · apply le_refl
+    apply inf_le_left
+  apply le_sup_left
 
 end
 
@@ -70,10 +172,10 @@ variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+  sorry -- Solution uses newly defined lemmas.
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+  sorry -- Solution uses newly defined lemmas.
 
 end
 
@@ -87,13 +189,31 @@ variable (a b c : R)
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  -- suggest_tactics
+  -- rwa [sub_nonneg]
+
+  -- search_proof
+  -- simp_all only [sub_nonneg]
+
+  -- aesop
+
+  rw [← sub_self a, sub_eq_add_neg, sub_eq_add_neg, add_comm, add_comm b]
+  apply add_le_add_left h
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  -- suggest_tactics
+  -- rwa [sub_nonneg] at h
+
+  -- search_proof
+  -- simp_all only [sub_nonneg]
+
+  -- aesop
+
+  rw [← add_zero a, ← sub_add_cancel b a, add_comm (b - a)]
+  apply add_le_add_left h
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  sorry -- Solution uses newly defined lemmas.
 
 end
 
@@ -106,7 +226,17 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  -- suggest_tactics
+  -- positivity
+
+  -- search_proof
+  -- positivity
+
+  have : 0 ≤ dist x y + dist y x := by
+    rw [← dist_self x]
+    apply dist_triangle
+  linarith [dist_comm x y]
+
+  -- aesop
 
 end
-
