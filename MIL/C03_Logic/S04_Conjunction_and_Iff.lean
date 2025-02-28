7e5ca492028aaf333bf513f2e097550ab11d2a37
psong <psong@caltech.edu>
2025-02-28 14:44:20 -0800

    C03 S04

--- MIL/C03_Logic/S04_Conjunction_and_Iff.lean
+++ MIL/C03_Logic/S04_Conjunction_and_Iff.lean
@@ -63,8 +63,19 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
 example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x :=
   fun h' ↦ h.right (le_antisymm h.left h')
 
-example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m :=
-  sorry
+example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
+  rcases h with ⟨h0, h1⟩
+  constructor
+  · exact h0
+  intro h2
+  apply h1
+  apply Nat.dvd_antisymm h0 h2
+
+  -- suggest_tactics
+
+  -- search_proof
+
+  -- aesop
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
   ⟨5 / 2, by norm_num, by norm_num⟩
@@ -101,15 +112,64 @@ example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y := by
 example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y :=
   ⟨fun h₀ h₁ ↦ h₀ (by rw [h₁]), fun h₀ h₁ ↦ h₀ (le_antisymm h h₁)⟩
 
-example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-  sorry
+example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y := by
+  -- search_proof
+  -- simp_all only [not_le, ne_eq, and_congr_right_iff]
+  -- intro a
+  -- apply Iff.intro
+  -- · intro a_1
+  --   apply Aesop.BuiltinRules.not_intro
+  --   intro a_2
+  --   subst a_2
+  --   simp_all only [le_refl, lt_self_iff_false]
+  -- · intro a_1
+  --   exact lt_of_le_of_ne a a_1
+
+  constructor
+  · rintro ⟨h0, h1⟩
+    constructor
+    · exact h0
+    intro h2
+    apply h1
+    rw [h2]
+  rintro ⟨h0, h1⟩
+  constructor
+  · exact h0
+  intro h2
+  apply h1
+
+  -- suggest_tactics
+  -- exact le_antisymm h0 h2
+
+  apply le_antisymm h0 h2
+
+  -- aesop
 
 theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
   have h' : x ^ 2 = 0 := by sorry
   pow_eq_zero h'
 
-example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 :=
-  sorry
+example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 := by
+
+  constructor
+  · intro h
+    constructor
+    · exact aux h
+    rw [add_comm] at h
+    exact aux h
+
+  -- search_proof
+  -- intro a
+  -- simp_all only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow, add_zero]
+
+  -- aesop
+
+  rintro ⟨rfl, rfl⟩
+
+  -- suggest_tactics
+  -- norm_num
+
+  norm_num
 
 section
 
@@ -130,15 +190,58 @@ theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y �
   rfl
 
 example : ¬Monotone fun x : ℝ ↦ -x := by
-  sorry
+
+  rw [not_monotone_iff]
+  use 0, 1
+
+  -- search_proof
+  -- simp_all only [zero_le_one, neg_zero, gt_iff_lt, Left.neg_neg_iff, zero_lt_one, and_self]
+
+  -- aesop
+
+  -- suggest_tactics
+  -- simp
+
+  norm_num
 
 section
 variable {α : Type*} [PartialOrder α]
 variable (a b : α)
 
 example : a < b ↔ a ≤ b ∧ a ≠ b := by
+  -- search_proof
+  -- simp_all only [ne_eq]
+  -- apply Iff.intro
+  -- · intro a_1
+  --   apply And.intro
+  --   · exact le_of_lt a_1
+  --   · apply Aesop.BuiltinRules.not_intro
+  --     intro a_2
+  --     subst a_2
+  --     simp_all only [lt_self_iff_false]
+  -- · intro a_1
+  --   obtain ⟨left, right⟩ := a_1
+  --   exact lt_of_le_of_ne left right
+
+  -- suggest_tactics
+  -- exact lt_iff_le_and_ne
+
   rw [lt_iff_le_not_le]
-  sorry
+  constructor
+  · rintro ⟨h0, h1⟩
+    constructor
+    · exact h0
+    intro h2
+    apply h1
+    rw [h2]
+  rintro ⟨h0, h1⟩
+  constructor
+  · exact h0
+  intro h2
+  apply h1
+  apply le_antisymm h0 h2
+
+  -- aesop
 
 end
 
@@ -147,11 +250,33 @@ variable {α : Type*} [Preorder α]
 variable (a b c : α)
 
 example : ¬a < a := by
+  -- search_proof
+  -- simp_all only [lt_self_iff_false, not_false_eq_true]
+
+  -- suggest_tactics
+  -- simp
+
+  -- aesop
+
   rw [lt_iff_le_not_le]
-  sorry
+  rintro ⟨h0, h1⟩
+  exact h1 h0
 
 example : a < b → b < c → a < c := by
+  -- search_proof
+  -- intro a_1 a_2
+  -- exact lt_trans a_1 a_2
+
   simp only [lt_iff_le_not_le]
-  sorry
+  rintro ⟨h0, h1⟩ ⟨h2, h3⟩
+  constructor
+  · apply le_trans h0 h2
+  intro h4
+  apply h1
+  apply le_trans h2 h4
+
+  -- suggest_tactics
+
+  -- aesop
 
 end
